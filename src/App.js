5f2d553b38e7018499384e7cb07efb6d524407fa
PranayZalke <pranayzalke111@gmail.com>
2023-02-10 15:46:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Axios from "axios";
+import {useState} from "react";
+import RecipeTile from './RecipeTile';
 function App() {
+  const [query, setquery] = useState("");
+  const [recipes, setrecipes] = useState([]);
+
+
+  const YOUR_APP_ID = `ce2d2862`;
+  const YOUR_APP_KEY = "cf7785fe0ebb15be2c1444dd10b79240";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+  //fun to get data from api
+  async function getRecipes() {
+   var result = await Axios.get(url);
+    setrecipes(result.data.hits);
+    console.log(result.data);
+  }
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Delicious Recipes Search 🍔🍟🥤</h1>
+      <form className="app_searchForm" onSubmit={onSubmit}>
+        <input 
+         type ="text" className='app_input'
+         placeholder="type in an ingredient"         value={query} 
+         onChange={(e) => setquery(e.target.value)}/>
+         <input className="app_submit" type="submit" value="Search"/>
+      </form>
+
+      <div className='app_recipes'>
+       {recipes.map((recipe) => {
+        return <RecipeTile recipe={recipe}/>
+       })}
+      </div>
     </div>
+    
   );
 }
 
---
+++ src/RecipeTile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./RecipeTile.css"
+
+export default function RecipeTile({recipe}) {
+  return (
+    <div className='recipeTile' onClick = {() =>{
+      window.open(recipe["recipe"]["url"]);
+    }} >
+        <img className="recipeTile_img" src={recipe["recipe"]["image"]}/>
+        <p className="recipeTile_name">{recipe["recipe"]["label"]}</p>
+      
+    </div>
+  );
+}
