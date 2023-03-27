2f75978aeffb26d50ce986fa0eba541a8e9a2869
PranayZalke <pranayzalke111@gmail.com>
2023-03-27 22:58:12 +0530

    some update

--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -4,9 +4,10 @@ import "./RecipeTile.css"
 export default function RecipeTile({recipe}) {
   return (
     <div className='recipeTile' onClick = {() =>{
-      window.open(recipe["recipe"]["url"]);
+         window.open(recipe["recipe"]["url"]);
     }} >
-    <img className="recipeTile_img" alt="Yummy desert" src={recipe["recipe"]["image"]}/>        <p className="recipeTile_name">{recipe["recipe"]["label"]}</p>
+          <img className="recipeTile_img" alt="altphoto" src={recipe["recipe"]["image"]}/>
+               <p className="recipeTile_name">{recipe["recipe"]["label"]}</p>
       
     </div>
   );
